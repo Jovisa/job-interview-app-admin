115c0359b4cc51feb33254774daf14160cd55cba
Jovisa <jsjovan94@gmail.com>
2018-01-17 05:39:55 +0100

    basic file structure for admin-app

---
+++ admin/src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class App extends React.Component {
+    render() {
+        return (
+          <div className="test">
+            <h1>Test</h1>
+          </div>
+        );
+      }
+}
+
+export default App;
