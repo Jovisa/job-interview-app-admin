7019f19bb3bca4e835412bf6ae316b9c99af90dc
Jovisa <jsjovan94@gmail.com>
2018-01-22 03:23:20 +0100

    creating new branch -modal

--- admin/src/components/main/reports/singleReport.js
+++ admin/src/components/main/reports/singleReport.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 export default class ReportsList extends React.Component {
-  
+
 
     render() {
         let reports = this.props.reports;
-        return(
-            reports.map((report, index )=> {
+        return (
+            reports.map((report, index) => {
                 let date = new Date(report.interviewDate);
                 let formatedDate = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}.`
-                return(
+                return (
                     <div className="row my-3" key={index}>
                         <div className="col col-12 col-md-4">{report.companyName}</div>
                         <div className="col col-12 col-md-4">{report.candidateName}</div>
@@ -17,14 +17,16 @@ export default class ReportsList extends React.Component {
                             <div className="row">
                                 <div className="col col-5">{formatedDate}</div>
                                 <div className="col col-3">{report.phase}</div>
-                                <div className="col col-4"><button>detal</button></div>
+                                <div className="col col-4">
+                                    <button>detal</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 );
             })
         );
-     
+
     }
 
 
